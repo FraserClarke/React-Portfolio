1b5bd3145bef6dc18177794ee1ab84e3a229cd7a
Mona Lisa <clarkefraser1@gmail.com>
2021-08-06 12:21:18 +1000

    updating about section, refining styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -22,10 +22,12 @@ export default function About() {
             <div className="col-md-3 col-sm-12 d-flex align-items-center justify-content-center">
               <div>
                 <img
+                  className="profilePictureAbout"
                   style={{
                     width: "220px",
                     margin: "0 auto",
                     display: "block",
+
                     clipPath: "circle(110px at center)",
                   }}
                   src={profile}
@@ -38,14 +40,43 @@ export default function About() {
               style={{ margin: "0 auto", padding: "0 25px" }}
             >
               <p className="aboutme-description">
-                Hi! My name is Fraser. I am currently studying full stack web
-                development at the University of Sydney
+                Full Stack Web Developer with a background in
+                Horticulture/Conservation and land management, which leverages
+                my skills of being a team player and having a strong work ethic
+                with great communication skills.
               </p>
+              <p className="aboutme-description">
+                I have recently completed a Certificate in Full Stack Web
+                Development at the University of Sydney. During my projects
+                throughout the course, I enjoyed both front end and back end
+                work, especially developing dynamically generated data utilizing
+                JavaScript, jQuery and React, then displaying that data and
+                refining it utilizing CSS resulting in a positive user friendly
+                experience.
+              </p>
+              <p className="aboutme-description">
+                I'm excited to harness my previous skills and creativity as
+                we’ll have recently acquired ones to further my career in Web
+                Development, providing better and exciting experiences for users
+                in the future.{" "}
+                <a href="#portfolio">
+                  <strong>View Work.</strong>
+                </a>
+              </p>
+              <br></br>
 
               <p className="aboutme-description">
-                I have a background in conservation & Land management and
-                horticulture. I enjoy gardening, and cultivating rare plants,
-                specifically orchids in my greenhouse.{" "}
+                <strong>Languages/ Technologies:</strong> JavaScript, HTML, CSS,
+                React.js, MERN stack, jQuery, Node.js, SQL, NoSQL, ES6, ES5,
+                Bootstrap, AJAX, Sequelize.js, Mongoose, Express.js, API’s,
+                PWA’s, ORM, Handlebars,js, Heroku.
+              </p>
+              <p className="aboutme-description">
+                <strong>Databases:</strong> MongoDB, JawsDB, IndexedDB, MySQL.
+              </p>
+              <p className="aboutme-description">
+                <strong>Tools:</strong> Postman, DevTools, Robo3T, GitHub,
+                KanBan, GitBash.{" "}
                 <a href="#portfolio">
                   <strong>View Work.</strong>
                 </a>
